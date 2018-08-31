b5280cdf4ce3e1b8ce4e47d80152ff901929131d
UnderSourceCode <undersourcecode@hotmail.co.jp>
2018-08-31 20:12:55 +0900

    Unauthorizedの処理を一時的に作成。

--- handlers/productsHandler.go
+++ handlers/productsHandler.go
@@ -1,16 +1,21 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 /* The products handler will be called when the user makes a GET request to the /products endpoint.
    This handler will return a list of products available for users to review */
 var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// Here we are converting the slice of products to JSON
-	payload, _ := json.Marshal(products)
-
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(payload))
+
+	// TODO: add token validate.
+	// status 401 unauthorized.
+	w.WriteHeader(401)
+	w.Write([]byte("your token is not authorized."))
+
+	// TODO: redo comment out.
+	// Here we are converting the slice of products to JSON
+	// payload, _ := json.Marshal(products)
+	// w.Write([]byte(payload))
 })
